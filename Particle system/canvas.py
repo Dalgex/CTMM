82f561b1119e3fd8c54b4fd7e5837f988d2fed85
Dalgex <valera-96@yandex.ru>
2019-10-29 12:35:31 +0300

    Changed canvas update

--- Particle system/canvas.py
+++ Particle system/canvas.py
@@ -9,18 +9,22 @@ class CanvasPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent, size=(800, 480))
         matplotlib.rc('axes', edgecolor='black', linewidth=1)
-        self._figure = Figure()
-        self._axes = self._figure.add_axes([0, 0, 1, 1])
-        self._canvas = FigureCanvas(self, id=-1, figure=self._figure)
-        self._figure.set_canvas(self._canvas)
-        self._scat = None
+        self.figure = Figure()
+        self._axes = self.figure.add_axes([0, 0, 1, 1])
+        self._axes.set(xlim=(0, 1), ylim=(0, 1))
+        self._scat = self._axes.scatter(x=[], y=[])
+        self._canvas = FigureCanvas(self, id=-1, figure=self.figure)
+        self.figure.set_canvas(self._canvas)
         box_sizer = wx.BoxSizer(wx.VERTICAL)
         box_sizer.Add(self._canvas, flag=wx.EXPAND | wx.TOP, border=6)
         self.SetSizer(box_sizer)
         self.Fit()
 
     def draw_markers(self, marker_positions, marker_sizes, marker_colors):
-        self._axes.clear()
-        self._scat = self._axes.scatter(x=marker_positions[:, 0], y=marker_positions[:, 1],
-                                        s=marker_sizes, facecolors=marker_colors)
-        self._canvas.draw()
+        self._scat.set_offsets(marker_positions)
+        self._scat.set_sizes(marker_sizes)
+        self._scat.set_facecolors(marker_colors)
+
+    def clear(self):
+        self._scat.remove()
+        self._scat = self._axes.scatter(x=[], y=[])
