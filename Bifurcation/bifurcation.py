aaff2f032fea050f90aa15d97812e06f7498bf5f
Dalgex <valera-96@yandex.ru>
2019-12-16 09:58:19 +0300

    Added two-parameter analysis

--- Bifurcation/bifurcation.py
+++ Bifurcation/bifurcation.py
@@ -81,13 +81,104 @@ def perform_one_param_analysis(f1, f2, param_dict, variable_param):
     plt.show()
 
 
+def is_zero(item):
+    return item < 1e-3
+
+
+def perform_two_param_analysis(f1, f2, param_dict, variable_params):
+    matrix = Matrix([f1, f2])
+    variables = Matrix([x, y])
+    jacobian = matrix.jacobian(variables)
+    det = jacobian.det()
+    trace = jacobian.trace()
+
+    y_f2_sol = solve(f2, y)[0]
+    param2_sol = solve(f1, variable_params[1])[0]
+    param2_trace_sol = solve(trace, variable_params[1])[0]
+    param1_trace_sol = solve(param2_sol - param2_trace_sol, variable_params[0])[0]
+    param2_det_sol = solve(det, variable_params[1])[0]
+    param1_det_sol = solve(param2_sol - param2_det_sol, variable_params[0])[0]
+
+    del param_dict[variable_params[0]]
+    del param_dict[variable_params[1]]
+    param1_trace_sol = param1_trace_sol.subs(get_params(param_dict))
+    param1_det_sol = param1_det_sol.subs(get_params(param_dict))
+    param2_sol = param2_sol.subs(get_params(param_dict))
+    y_f2_sol = y_f2_sol.subs(get_params(param_dict))
+    jacobian = jacobian.subs(get_params(param_dict))
+
+    x_array = np.linspace(0, 1, 1000)
+    y_f2_func = lambdify(x, y_f2_sol, 'numpy')
+    y_array = y_f2_func(x_array)
+    x_list, y_list = set_variable_values(x_array, y_array)
+
+    param1_trace_func = lambdify(variables, param1_trace_sol, 'numpy')
+    param1_det_func = lambdify(variables, param1_det_sol, 'numpy')
+    variables = np.append(variables, variable_params[0])
+    param2_func = lambdify(variables, param2_sol, 'numpy')
+
+    param1_trace_list = []
+    for i in range(len(x_list)):
+        param1_trace_list.append(param1_trace_func(x_list[i], y_list[i]))
+
+    param2_trace_list = []
+    for i in range(len(x_list)):
+        param2_trace_list.append(param2_func(x_list[i], y_list[i], param1_trace_list[i]))
+
+    param1_det_list = []
+    for i in range(len(x_list)):
+        param1_det_list.append(param1_det_func(x_list[i], y_list[i]))
+
+    param2_det_list = []
+    for i in range(len(x_list)):
+        param2_det_list.append(param2_func(x_list[i], y_list[i], param1_det_list[i]))
+
+    eigen1, eigen2 = jacobian.eigenvals()
+    variables = np.append(variables, variable_params[1])
+    eigen1_func = lambdify(variables, eigen1, 'numpy')
+    eigen2_func = lambdify(variables, eigen2, 'numpy')
+    tb_points = []
+    for i in range(len(x_list)):
+        eig1 = eigen1_func(x_list[i], y_list[i], param1_trace_list[i], param2_trace_list[i])
+        eig2 = eigen2_func(x_list[i], y_list[i], param1_trace_list[i], param2_trace_list[i])
+        if not np.isnan(eig1) and not np.isnan(eig2) and is_zero(eig1) and is_zero(eig2):
+            tb_points.append([param2_trace_list[i], param1_trace_list[i]])
+
+    param1_det_diff = param1_det_sol.diff(x)
+    param1_det_diff_func = lambdify(variables[:2], param1_det_diff, 'numpy')
+    diff_list = []
+    for i in range(len(x_list)):
+        if is_zero(np.abs(param1_det_diff_func(x_list[i], y_list[i]))):
+            diff_list.append(x_list[i])
+
+    x_c = sum(diff_list) / len(diff_list)
+    y_c = y_f2_func(x_c)
+    param1_c = param1_det_sol.subs({x: x_c, y: y_c})
+    param2_c = param2_sol.subs({x: x_c, y: y_c, variable_params[0]: param1_c})
+
+    plt.plot(param2_trace_list, param1_trace_list, "--", label='Neutrality line')
+    plt.plot(param2_det_list, param1_det_list, label='Multiplicity line')
+    plt.plot(param2_c, param1_c, 'go', label='C')
+
+    if tb_points:
+        x_axis, y_axis = np.transpose(tb_points)
+        plt.plot(x_axis, y_axis, 'ro', label='Takens-Bogdanov')
+
+    plt.xlabel(f'{variable_params[1]}')
+    plt.ylabel(f'{variable_params[0]}')
+    plt.ylim(-0.01, 0.02)
+    plt.legend()
+    plt.show()
+
+
 def main():
     param_dict = {k_3: 0.002, k_1: 0.01, k1: 0.12, k2: 0.95, k3: 0.0032}
     z = 1 - x - 2 * y
     dx_dt = k1 * z - k_1 * x - k3 * x * z + k_3 * y - k2 * x * (z ** 2)
     dy_dt = k3 * x * z - k_3 * y
     param = k1
-    perform_one_param_analysis(dx_dt, dy_dt, param_dict, param)
+    perform_one_param_analysis(dx_dt, dy_dt, param_dict.copy(), param)
+    perform_two_param_analysis(dx_dt, dy_dt, param_dict.copy(), [k_1, k1])
 
 
 if __name__ == "__main__":
