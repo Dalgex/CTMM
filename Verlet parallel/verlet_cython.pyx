30cc3ff9c84582349c4b659c1e7d6b08d91a7b68
Dalgex <valera-96@yandex.ru>
2020-01-08 18:01:43 +0300

    Made execution of verlet cython on the entire time interval

--- Verlet parallel/verlet_cython.pyx
+++ Verlet parallel/verlet_cython.pyx
@@ -5,13 +5,30 @@ DTYPE = np.double
 ctypedef np.double_t DTYPE_t
 
 
-def calculate_verlet_cython(np.ndarray[DTYPE_t, ndim=2] data, double delta_t):
+def calculate_verlet_cython(np.ndarray[DTYPE_t, ndim=2] data,
+                            double max_time, int tick_count):
+    cdef double delta_t = max_time / tick_count
     cdef int N = len(data)
+    cdef int M = tick_count
     cdef DTYPE_t[:, :] prev_data = deepcopy(data)
     cdef DTYPE_t[:, :] prev_accs = np.zeros((N, 2), dtype=DTYPE)
-    _update_coordinates(data, prev_data, prev_accs, delta_t)
-    _update_speed(data, prev_accs, delta_t)
-    return data
+    cdef DTYPE_t[:, :, :] result = np.zeros((M, N, len(data[0])), dtype=DTYPE)
+    _copy_data(data, result, 0)
+    cdef int i
+
+    for i in range(1, M):
+        prev_data = deepcopy(data)
+        _update_coordinates(data, prev_data, prev_accs, delta_t)
+        _update_speed(data, prev_accs, delta_t)
+        _copy_data(data, result, i)
+    return result
+
+
+def _copy_data(DTYPE_t[:, :] data, DTYPE_t[:, :, :] result, int index):
+    cdef int i, j
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            result[index, i, j] = data[i, j]
 
 
 def _calculate_acceleration(DTYPE_t[:, :] data, int index):
